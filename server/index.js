a991d953fad72b08a3db6011a06c2f19abe3be0a
Rodrigo Thauby <rthauby@gmail.com>
2019-08-05 10:59:22 -0400

    adding models

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
